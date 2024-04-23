3df4ad094e4aa09f37adc087c96599d4f2d5cb83
Sebasrv22 <a01656563@tec.mx>
2024-04-22 19:30:47 -0600

    matplot

--- hola.py
+++ hola.py
@@ -8,13 +8,20 @@ st.write("Titanic gráficas")
 
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as ptl
+import matplotlib.pyplot as plt
 
 titanic_link = '/Users/sebastianromero/Desktop/graficas/Titanic.csv'
 titanic_data = pd.read_csv(titanic_link)
 
 st.dataframe(titanic_data)
 st.header("Data Description")
+
+fig, ax = plt.subplots()
+ax.hist(titanic_data["Fare"])
+st.header("Histograma del Titanic")
+st.pyplot(fig)
+
+
 
 
 
