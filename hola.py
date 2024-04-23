8d6876cbd8e06f459b5e8c74d74ad9724d393929
Sebasrv22 <a01656563@tec.mx>
2024-04-22 20:27:24 -0600

    hola

--- hola.py
+++ hola.py
@@ -1,27 +1,19 @@
-# Importamos la librería Streamlit
-import streamlit as st
-
-# Crear el título para la aplicación web
-st.title("Mi Primera App")
-st.header ("Sebastián Romero Velasco")
-st.write("Titanic gráficas")
-
+#importamos la biblioteca streamlit
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
+import matplotlib as plt
+
+#creamos el título de la app
+st.title("Mi primera App")
+st.header("Demo Titanic App")
+st.write("Graficas del dataset")
 
 titanic_link = '/Users/sebastianromero/Desktop/graficas/Titanic.csv'
-titanic_data = pd.read_csv(titanic_link)
-
+titanic_data =pd.read_csv(titanic_link)
 st.dataframe(titanic_data)
-st.header("Data Description")
 
 fig, ax = plt.subplots()
 ax.hist(titanic_data["Fare"])
 st.header("Histograma del Titanic")
 st.pyplot(fig)
-
-
-
-
 
